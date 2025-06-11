0953f0c1e98ccdbce4a20f8f86a88236eb29adde
grgustiawan <galihrakagustiawan@gmail.com>
2025-06-11 08:45:53 +0700

    Fix: CodeQL + Eslint Analysis to ignore .eslintrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,13 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.eslintrc.js',
+    ],
+  },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
