62ae299f29b0dd0dc6e8e2ff9db93ce4a4e7d400
grgustiawan <galihrakagustiawan@gmail.com>
2025-06-11 08:33:05 +0700

    Fix: Update Code Analysis with eslint linter

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
